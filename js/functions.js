baf573c9154f81f09da8e2fd749a3513671d17a8
Hanna Nesterova <nesterova.hanna@gmail.com>
2022-12-28 23:39:23 +0100

    Create variable computerMove

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,11 @@
 function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
-
     document.getElementById('messages').appendChild(div);
 }
-printMessage('Я грав каменем! Якщо ти грав папером,то ти виграв!');
+
+let computerMove = `камінь`;
+printMessage('Я грав' + computerMove + '! Якщо ти грав папером,то ти виграв!');
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
