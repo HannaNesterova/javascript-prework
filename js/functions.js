81975ce250ba40f9eda11e8ecdb4e3a86a51a24b
Hanna Nesterova <nesterova.hanna@gmail.com>
2022-12-29 00:05:27 +0100

    Replace virables to script.js

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,12 @@
-
-let computerMove = 'камінь';
-let playerMove = 'папір';
-
-function printMessage(msg){
+function printMessage(msg) {
 	let div = document.createElement('div');
 	div.innerHTML = msg;
+
     document.getElementById('messages').appendChild(div);
 }
 
-printMessage('Я грав' + computerMove + '! Якщо ти грав папером,то ти виграв!');
 
-function clearMessages(){
+
+function clearMessages() {
 	document.getElementById('messages').innerHTML = '';
 }
--- js/script.js
+++ js/script.js
@@ -1,15 +1,4 @@
-
 let computerMove = 'камінь';
 let playerMove = 'папір';
 
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-    document.getElementById('messages').appendChild(div);
-}
-
-printMessage('Я грав' + computerMove + '! Якщо ти грав папером,то ти виграв!');
-
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
-}
+printMessage('Я обрав ' + computerMove + '! Якщо ти обрав ' + playerMove + ',то ти виграв!');
