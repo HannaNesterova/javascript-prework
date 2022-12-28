fb78c7245962a53f2fccadf2c516f923aa5590ec
Hanna Nesterova <nesterova.hanna@gmail.com>
2022-12-28 23:18:00 +0100

    call the function printMessage

--- js/functions.js
+++ js/functions.js
@@ -4,6 +4,7 @@ function printMessage(msg){
 
     document.getElementById('messages').appendChild(div);
 }
+printMessage('Я грав каменем! Якщо ти грав папером,то ти виграв!');
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
