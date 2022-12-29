6c219ed63195bcb7f20bac2af90c91e30f4380f9
Hanna Nesterova <nesterova.hanna@gmail.com>
2022-12-29 17:40:20 +0100

    Create condition for playerInput

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ let computerMove = 'невідомий рух';
 if (roundNumber === 1){
     computerMove = 'камінь';
 }
-
 printMessage('мій рух :' + computerMove);
 
+let playerInput = prompt ('Вибери свій рух! 1 - це камінь, 2 - це папір, 3 - це ножиці');
+console.log('Гравець обрав :' + playerInput);
+
+let playerMove = 'невідомий рух';
+if (playerInput === '1'){
+    playerMove = 'камінь';
+}
+printMessage('твій рух :' + playerMove);
