7f26480bc2f586228a6a10711589937b9b7db2e3
Hanna Nesterova <nesterova.hanna@gmail.com>
2022-12-29 17:53:26 +0100

    Create conditions for else if and else for player and computre

--- js/script.js
+++ js/script.js
@@ -6,13 +6,30 @@ let computerMove = 'невідомий рух';
 if (roundNumber === 1){
     computerMove = 'камінь';
 }
+else if (roundNumber === 2){
+    computerMove = 'папір';
+}
+else {
+    computerMove = 'ножиці';
+}
+
+
+
 printMessage('мій рух :' + computerMove);
 
 let playerInput = prompt ('Вибери свій рух! 1 - це камінь, 2 - це папір, 3 - це ножиці');
 console.log('Гравець обрав :' + playerInput);
 
 let playerMove = 'невідомий рух';
-if (playerInput === '1'){
+if (playerInput === '1') {
     playerMove = 'камінь';
 }
+else if (playerInput === '2') {
+    playerMove = 'папір';
+}
+
+else {
+    playerMove = 'ножиці';
+}
+
 printMessage('твій рух :' + playerMove);
