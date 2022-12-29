2b72675cd940e69420119dd27c4f801c1d5f38e1
Hanna Nesterova <nesterova.hanna@gmail.com>
2022-12-29 15:12:44 +0100

    Create Math.random for rendomFraction

--- js/script.js
+++ js/script.js
@@ -2,3 +2,15 @@ let computerMove = 'камінь';
 let playerMove = 'папір';
 
 printMessage('Я обрав ' + computerMove + '! Якщо ти обрав ' + playerMove + ',то ти виграв!');
+
+let randomFraction = Math.random();
+
+printMessage('Випадково згенероване число: ' + randomFraction);
+
+let calculation = randomFraction * 3 + 1;
+
+printMessage('Число помножене на 3 та збільшене на 1: ' + calculation);
+
+let roundNumber = Math.floor(calculation);
+
+printMessage('Сума після заокругдення числа до нижчого рівня: ' + roundNumber);
