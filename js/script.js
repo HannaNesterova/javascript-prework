e83ddc5f204a44950c3aa992cbc4ef7f03bf09e9
Hanna Nesterova <nesterova.hanna@gmail.com>
2022-12-30 17:31:24 +0100

    Call the general  function

--- js/script.js
+++ js/script.js
@@ -1,54 +1,55 @@
 // Computer choises
 function playGame () {
     let roundNumber = Math.floor(Math.random() * 3 + 1);
-let computerMove = getMoveName(roundNumber);
+    let computerMove = getMoveName(roundNumber);
 
-function getMoveName(argMoveId){
-    if (argMoveId === 1){
-        return 'камінь';
+    function getMoveName(argMoveId){
+        if (argMoveId === 1){
+            return 'камінь';
+        }
+        else if (argMoveId === 2){
+            return 'папір';
+        }
+        else if(argMoveId === 3){
+            return 'ножиці';
+        } 
+        else(
+            alert('Використовуй цифри')
+        )
     }
-    else if (argMoveId === 2){
-        return 'папір';
-    }
-    else if(argMoveId === 3){
-        return 'ножиці';
-    } 
-    else(
-        alert('Використовуй цифри')
-    )
-  
-}
 
-printMessage('Рух компютера: ' + computerMove);
+    printMessage('Рух компютера: ' + computerMove);
 
-// Player
+    // Player
 
-let playerInput = prompt ('Вибери свій рух! 1 - це камінь, 2 - це папір, 3 - це ножиці');
-let playerMove = getMoveName(Number(playerInput));
-printMessage('Tвій рух :' + playerMove);
+    // let playerInput = prompt ('Вибери свій рух! 1 - це камінь, 2 - це папір, 3 - це ножиці');
+    let playerMove = getMoveName(Number(playerInput));
+    printMessage('Tвій рух :' + playerMove);
 
-let argComputerMove = computerMove;
-let argPlayerMove = playerMove;
-function displayResult (argComputerMove , argPlayerMove){
-    printMessage('Компютер обрав ' + argComputerMove + ', а ти обрав ' + argPlayerMove);
+    let argComputerMove = computerMove;
+    let argPlayerMove = playerMove;
+    function displayResult (argComputerMove , argPlayerMove){
+        printMessage('Компютер обрав ' + argComputerMove + ', а ти обрав ' + argPlayerMove);
+            if( argComputerMove === 'камінь' && argPlayerMove === 'папір'){
+                printMessage('Вітаю, ти переміг!');
+            }
+            else if( argComputerMove === 'ножиці' && argPlayerMove === 'камінь'){
+                printMessage('Вітаю, ти переміг!');
+            }
+            else if( argComputerMove === 'папір' && argPlayerMove === 'ножиці'){
+            printMessage('Вітаю, ти переміг!');
+            }
+            else if( argComputerMove === 'папір' && argPlayerMove === 'папір' || computerMove === 'ножиці' && playerMove === 'ножиці' || computerMove === 'камінь' && playerMove === 'камінь'){
+            printMessage('Нечія,спробуйте ще раз.');
+            }
+            else{
+            printMessage('Нажаль ти програв,спробуйте ще...');
+            }
+        }
 
-    if( argComputerMove === 'камінь' && argPlayerMove === 'папір'){
-        printMessage('Вітаю, ти переміг!');
-      }
-      else if( argComputerMove === 'ножиці' && argPlayerMove === 'камінь'){
-        printMessage('Вітаю, ти переміг!');
-     }
-     else if( argComputerMove === 'папір' && argPlayerMove === 'ножиці'){
-       printMessage('Вітаю, ти переміг!');
-      }
-     else if( argComputerMove === 'папір' && argPlayerMove === 'папір' || computerMove === 'ножиці' && playerMove === 'ножиці' || computerMove === 'камінь' && playerMove === 'камінь'){
-       printMessage('Нечія,спробуйте ще раз.');
-     }
-     
-     else{
-       printMessage('Нажаль ти програв,спробуйте ще...');
-    }
-}
 displayResult (argComputerMove , argPlayerMove);
+playGame(playerInput);
 }
-playGame();
+
+
+playGame(3);
