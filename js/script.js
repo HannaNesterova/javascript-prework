a5ef217f45a2f79dfa5cfecacf84d0b520581b8e
Hanna Nesterova <nesterova.hanna@gmail.com>
2022-12-30 01:09:04 +0100

    Try to solve a problem

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 // Computer choises
+let roundNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove = getMoveName(roundNumber);
+
 function getMoveName(argMoveId){
-    if (argMoveId === '1'){
+    if (argMoveId === 1){
         return 'камінь';
     }
-    else if (argMoveId === '2'){
+    else if (argMoveId === 2){
         return 'папір';
     }
-    else {
+    else if(argMoveId === 3){
         return 'ножиці';
-    }
+    } 
+    else(
+        alert('Використовуй цифри')
+    )
+  
 }
 
-let roundNumber = Math.floor(Math.random() * 3 + 1);
 
-
-let computerMove = getMoveName(roundNumber);
 printMessage('Рух компютера: ' + computerMove);
 
 
@@ -25,39 +29,27 @@ let playerInput = prompt ('Вибери свій рух! 1 - це камінь, 
 let playerMove = getMoveName(playerInput);
 printMessage('Tвій рух :' + playerMove);
 
-// if (playerInput === '1') {
-//     playerMove = 'камінь';
-// }
-// else if (playerInput === '2') {
-//     playerMove = 'папір';
-// }
-
-// else {
-//     playerMove = 'ножиці';
-// }
 let argComputerMove = computerMove;
 let argPlayerMove = playerMove;
 function displayResult (argComputerMove , argPlayerMove){
-    printMessage('Компютер обрав ' + argComputerMove + ' а ти обрав ' + argPlayerMove);
+    printMessage('Компютер обрав ' + argComputerMove + ', а ти обрав ' + argPlayerMove);
+
+    if( argComputerMove === 'камінь' && argPlayerMove === 'папір'){
+        printMessage('Вітаю, ти переміг!');
+      }
+      else if( argComputerMove === 'ножиці' && argPlayerMove === 'камінь'){
+        printMessage('Вітаю, ти переміг!');
+     }
+     else if( argComputerMove === 'папір' && argPlayerMove === 'ножиці'){
+       printMessage('Вітаю, ти переміг!');
+      }
+     else if( argComputerMove === 'папір' && argPlayerMove === 'папір' || computerMove === 'ножиці' && playerMove === 'ножиці' || computerMove === 'камінь' && playerMove === 'камінь'){
+       printMessage('Нечія,спробуйте ще раз.');
+     }
+     
+     else{
+       printMessage('Нажаль ти програв,спробуйте ще...');
+    }
 }
 displayResult (argComputerMove , argPlayerMove);
 
-// if( computerMove === 'камінь' && playerMove === 'папір'){
-//     printMessage('Вітаю, ти переміг!');
-//   }
-//   else if( computerMove === 'ножиці' && playerMove === 'камінь'){
-//     printMessage('Вітаю, ти переміг!');
-//   }
-//   else if( computerMove === 'папір' && playerMove === 'ножиці'){
-//     printMessage('Вітаю, ти переміг!');
-//   }
-//   else if( computerMove === 'папір' && playerMove === 'папір' || computerMove === 'ножиці' && playerMove === 'ножиці' || computerMove === 'камінь' && playerMove === 'камінь'){
-//     printMessage('Нечія,спробуйте ще раз.');
-//   }
-//   else if( playerInput < '1' || playerInput  > '3' ) {
-//    alert('Упс,я не знаю такої цифри, обери будь ласка між цифрами 1,2,3!');
-//   } 
-  
-//   else{
-//     printMessage('Нажаль ти програв,спробуйте ще...');
-//   }
